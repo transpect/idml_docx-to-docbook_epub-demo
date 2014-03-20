7c55dfdf69418d94db086d8a1823cffe591a1ccc
pglatza <pglatza@d851d441-0421-4803-b127-45cb279a3ef2>
2014-03-20 06:40:31 +0000

    docx2idml

---
+++ transpect-demo/trunk/adaptions/common/calabash/localdefs.sh
@@ -0,0 +1,10 @@
+LOCALDIR="$( cd -P "$(dirname $( readlink -f "$0" ))" && pwd )"
+
+case "`hostname`" in
+  don) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  pollux) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  aspect) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
+  worker0) JAVA=/data/ASSP/ASSP_chroot/usr/local/bin/java16;;
+esac
+
+CLASSPATH=$LOCALDIR/my-ext/MyExt.class
