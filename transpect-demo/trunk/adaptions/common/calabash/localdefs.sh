06556a87c4aab867cb10c47b1be966091d1f4b7b
mkraetke <mkraetke@d851d441-0421-4803-b127-45cb279a3ef2>
2015-02-04 14:21:59 +0000

    modify epubcheck path

--- transpect-demo/trunk/adaptions/common/calabash/localdefs.sh
+++ transpect-demo/trunk/adaptions/common/calabash/localdefs.sh
@@ -1,4 +1,5 @@
 LOCALDIR="$( cd -P "$(dirname $( readlink -f "$0" ))" && pwd )"
+CFG=$ADAPTIONS_DIR/common/calabash/xproc-config.xml
 
 case "`hostname`" in
   don) JAVA=/usr/lib/jvm/java-6-sun/bin/java;;
@@ -7,4 +8,3 @@ case "`hostname`" in
   worker0) JAVA=/data/ASSP/ASSP_chroot/usr/local/bin/java16;;
 esac
 
-CLASSPATH=$LOCALDIR/my-ext/MyExt.class
